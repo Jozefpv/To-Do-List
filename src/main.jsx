061e2a29462d84ead28fddc08d24f3167f326a94
Jozefpv <jozefpv99@gmail.com>
2023-05-16 14:15:28 +0100

    cambios en el fondo

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <div style={{backgroundColor:"black", width:"100vw", height:"100vh", color:"white"}}>
+  <div style={{backgroundColor:"black", color:"white"}}>
     <App />
   </div>,
 )
